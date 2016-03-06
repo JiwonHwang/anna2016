1fd3669a1ebff403e5906eede3be8188bf673802
JiwonHwang <rhasu2003@gmail.com>
2016-03-06 10:50:14 +0900

    add tinymce

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.utils import timezone
+from tinymce.models import HTMLField
 
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
-    text = models.TextField()
+    text = models.HTMLField()
     image = models.ImageField(blank=True)
     created_date = models.DateTimeField(
         default=timezone.now)

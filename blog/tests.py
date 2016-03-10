301e09f48ea1cadd7a97b7a054574c26ec2d065b
Jiwon Hwang <rhasu2003@gmail.com>
2016-03-10 17:54:45 +0900

    first tests.py commit

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.utils import timezone
+# from tinymce.models import HTLMField - Need it too?
+
+from .models import Post
+
+
+class PostMethodTests(TestCase):
+
+	def test_was_published_recently_with_future_question(self):
+		"""
+		was_published_recently() should return False for questions whose
+		pub_date is in the future.
+		"""
+		# To do 
+		# 1. migrate 
+		# 2. check bug in terminal 
+		# 3. revise tests.py here 

d756031c94d54f6407abc4a8e276ec1759d0ba5e
Jiwon Hwang <rhasu2003@gmail.com>
2016-03-10 20:05:21 +0900

    tests.py import datetime, fix typo @line16: deltatime.timedelta -> datetime.timedelta

--- blog/tests.py
+++ blog/tests.py
@@ -1,7 +1,7 @@
-from django.test import TestCase
+import datetime
 
+from django.test import TestCase
 from django.utils import timezone
-# from tinymce.models import HTLMField - Need it too?
 
 from .models import Post
 
@@ -13,6 +13,6 @@ class PostMethodTests(TestCase):
 		was_published_recently() should return False for posts whose
 		pubblished_date is in the future.
 		"""
-		time = timezone.now() + deltatime.timedelta(days=30)
+		time = timezone.now() + datetime.timedelta(days=30)
 		future_post = Post(published_date=time)
 		self.assertEqual(future_post.was_published_recently(), False)

61da5bd54c0e49cea8218f8b7c6f68e350435a3d
Jiwon Hwang <rhasu2003@gmail.com>
2016-03-10 17:55:28 +0900

    blog/migrations/0007

---
+++ blog/migrations/0007_auto_20160310_1725.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import tinymce.models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('blog', '0006_delete_mymodel'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='post',
+            name='text',
+            field=tinymce.models.HTMLField(),
+        ),
+    ]
